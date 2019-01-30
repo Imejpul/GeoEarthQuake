c2e6301e4a6999f3760b22d676b46c2fbd1855c7
Iván Mejía <ivanmejiapulido@gmail.com>
2019-01-30 12:08:08 +0000

    Set up básico

---
+++ GeoEarthQuake/GeoEarthQuake/EarthQuakeTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  EarthQuakeTableViewCell.swift
+//  GeoEarthQuake
+//
+//  Created by user148117 on 30/01/2019.
+//  Copyright © 2019 Imejpul. All rights reserved.
+//
+
+import UIKit
+
+class EarthQuakeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tituloTerremoto: UILabel!
+    
+    @IBAction func botonVerPulsado(_ sender: Any) {
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
